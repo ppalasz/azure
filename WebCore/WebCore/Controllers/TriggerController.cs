1a8aa32b8abf09fc7e80980d0f9184a485972b33
Paweł Pałasz <ppalasz@poczta.onet.pl>
2019-02-16 13:48:35 +0100

    trigger

---
+++ WebCore/WebCore/Controllers/TriggerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TriggerController : ControllerBase
+    {
+
+        // GET api/trigger/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "get "+ id;
+        }
+
+       
+    }
+}
